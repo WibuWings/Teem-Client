7b78f984ac6e6b2f3eef1cd4690feb7fa35df039
Minhthang1506 <19522216@gm.uit.edu.vn>
2023-01-09 23:43:02 +0700

    replace .env base path

--- src/providers/Socket.tsx
+++ src/providers/Socket.tsx
@@ -1,6 +1,7 @@
+import { BASE_PATH } from '@/config'
 import React, { ReactChildren, ReactElement, useMemo } from 'react'
 import { io, Socket } from 'socket.io-client'
-const socket = io('http://localhost:5000')
+const socket = io(BASE_PATH)
 export const SocketContext = React.createContext<{ socket: Socket<any, any> }>({
   socket: socket,
 })
